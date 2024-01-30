446f618bce80eea0e87c71d0eebd317e469b3203
ulisescastell <ulisescastell6@gmail.com>
2024-01-30 20:16:43 +0100

    add circle menu function

---
+++ src/main/kotlin/otherFunctions.kt
@@ -0,0 +1,10 @@
+fun menu () {
+    println("\n=== Menú de Gestió de Cercles ===")
+    println("1. Crear un cercle amb un radi i color determinats")
+    println("2. Crear un cercle amb un radi determinat")
+    println("3. Mostrar l'últim cercle creat")
+    println("4. Mostrar el perímetre de l'últim cercle creat")
+    println("5. Mostrar l'àrea de l'últim cercle creat")
+    println("6. Sortir")
+    println("Seleccioneu una opció: ")
+}
